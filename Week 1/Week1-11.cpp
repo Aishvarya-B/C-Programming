aac97f8a77024878a16d5bfd47ae3b2f0bcc875e
Aishvarya-B <143435621+Aishvarya-B@users.noreply.github.com>
2023-10-02 16:37:59 +0530

    Update Week1-11.cpp

--- Week 1/Week1-11.cpp
+++ Week 1/Week1-11.cpp
@@ -7,7 +7,7 @@ float p,r,t,n,si,ci;
 printf("enter p,r,t,n");
 scanf("%f%f%f%f",&p,&r,&t,&n);
 si=(p*r*t)/100;
-ci=(pow((p+(r/n)),(n*t)))-p;
+ci=p*(pow((1+(r/n)),(n*t)))-p;
 printf("si and ci are %f, %f",si,ci);
 return 0;
 }
