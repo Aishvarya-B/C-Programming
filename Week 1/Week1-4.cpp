5c3d507d5a4c58db06e495e0f1a367e0386fd33b
Aishvarya-B <143435621+Aishvarya-B@users.noreply.github.com>
2023-10-18 18:25:49 +0530

    Delete Week 1/Week1-4.cpp

--- Week 1/Week1-4.cpp
+++
@@ -1,9 +0,0 @@
-/*percentage for 5 different subject*/
-#include<stdio.h>
-int main()
-{float a,b,c,d,e,p;
-scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
-p=(a+b+c+d+e)/5;
-printf("Percentage is %f %% ",p);
-return 0;
-}
