db780211de9912453eb15c1326374cde4aa963da
Aishvarya-B <143435621+Aishvarya-B@users.noreply.github.com>
2023-10-18 18:41:27 +0530

    Adding pp programs

---
+++ pp4.cpp
@@ -0,0 +1,18 @@
+/*Printing patterns 2*/
+#include<stdio.h>
+int main()
+{
+	int i,j,k,n;
+	printf("Enter n");
+	scanf("%d",&n);
+	for (i=0;i<n;i++)
+	{
+		for(j=0;j<n-i;j++)
+		printf("* ");
+		for(k=0;k<i;k++)
+		printf(" ");
+		printf("\n");
+		
+	}
+	return 0;
+}
