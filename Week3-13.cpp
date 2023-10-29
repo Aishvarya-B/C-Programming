ccbc2b4abdf861545a7fd9c311ab05fd14e8e2f9
Aishvarya B <143435621+Aishvarya-B@users.noreply.github.com>
2023-10-29 19:07:11 +0530

    Changed to method using integers

--- Week3-13.cpp
+++ Week3-13.cpp
@@ -1,15 +1,15 @@
 /*decimal to binary*/
 #include<stdio.h>
+#include<math.h>
 int main()
 {
-	int i,d,bin=0;
-	char b="";
+	int i,d,bin=0,j;
+	
 	printf("Enter a number");
 	scanf("%d",&d);
-	for(i=d;i>0;i/=2)
-	b=b+char(i%2));
-	printf("%c \n",b);
-
+	for(i=d,j=0;i>0;i/=2,j++)
+	bin=bin+(i%2)*pow(10,j);
+	
 	
 	printf("%d in binary is %d",d,bin);
 	return 0;
