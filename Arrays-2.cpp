97824b6265769b8bd4a815e3fa07fde9e047c051
Aishvarya B <143435621+Aishvarya-B@users.noreply.github.com>
2023-11-03 23:29:33 +0530

    Update Arrays-2.cpp

--- Arrays-2.cpp
+++ Arrays-2.cpp
@@ -1,11 +1,15 @@
 #include<stdio.h>
 int main()
 {
-	int a[5],i,t,p,s,j,n=5;
+	int i,t,p,s,j,n;
+	printf("Enter n");
+	scanf("%d",&n);
+	int a[n];
+	
 	printf("Enter the array elements");
-	for (i=0;i<5;i++)
+	for (i=0;i<n;i++)
 	scanf("%d",&a[i]);
-	for (i=0;i<5;i++)
+	for (i=0;i<n;i++)
 	{
 		for(j=0;j<n-1-i;j++)
 		if(a[j+1]<a[j])
@@ -14,6 +18,6 @@ int main()
 		a[j+1]=t;
 		}
 	}
-	for (i=0;i<5;i++)
+	for (i=0;i<n;i++)
 	printf("%d ",a[i]);
 }
