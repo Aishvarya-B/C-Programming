ab6023866489e8141c4ea06abd1fbd56d2c0538f
Aishvarya B <143435621+Aishvarya-B@users.noreply.github.com>
2023-11-06 23:28:30 +0530

    Create Arrays-15.cpp

---
+++ Arrays-15.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+	int i,t,p,s,j,n;
+	printf("Enter n");
+	scanf("%d",&n);
+	int a[n];
+	
+	printf("Enter the array elements");
+	for (i=0;i<n;i++)
+	scanf("%d",&a[i]);
+	for (i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-1-i;j++)
+		if(a[j+1]<a[j])
+		{t=a[j];
+		a[j]=a[j+1];
+		a[j+1]=t;
+		}
+	}
+	
+	for(i=0;i<n/2+1;i++)
+	{if(a[i]==a[n/2+i])
+	{printf("%d",a[i]);
+	break;
+	}
+	}
+}
